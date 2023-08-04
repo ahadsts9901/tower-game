2da51907ee96b2b7ff7c6fb6c62c154e2510f76b
Muhammad Ahad <120649081+ahadsts9901@users.noreply.github.com>
2023-08-04 05:14:20 -0700

    functions done

--- main.js
+++ main.js
@@ -1,32 +1,86 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var cards = document.querySelectorAll('.block');
+let gameOver = false;
 
-  cards.forEach(function(card) {
-    card.addEventListener('dragstart', dragStart);
-    card.addEventListener('dragover', dragOver);
-    card.addEventListener('drop', drop);
+  function load() {
+    var towers = document.querySelectorAll('.tower');
+
+    towers.forEach(function (tower) {
+      var blocks = tower.querySelectorAll('.block');
+      var lastBlock = blocks[blocks.length - 1];
+
+      blocks.forEach(function (block, index) {
+        if (block === lastBlock && !gameOver) {
+          block.setAttribute('draggable', 'true');
+          block.addEventListener('dragstart', dragStart);
+        } else {
+          block.setAttribute('draggable', 'false');
+        }
+
+        block.addEventListener('dragover', dragOver);
+        block.addEventListener('drop', drop);
+      });
+    });
+  }
+
+  document.addEventListener('DOMContentLoaded', function () {
+    load();
   });
-});
 
-function allowDrop(event) {
-  event.preventDefault();
-}
+  function allowDrop(event) {
+    event.preventDefault();
+  }
 
-function dragStart(event) {
-  event.dataTransfer.setData('text/plain', event.target.id);
-}
+  function dragStart(event) {
+    event.dataTransfer.setData('text/plain', event.target.id);
+  }
 
-function dragOver(event) {
-  event.preventDefault();
-}
+  function dragOver(event) {
+    event.preventDefault();
+  }
 
-function drop(event) {
-  event.preventDefault();
-  var cardId = event.dataTransfer.getData('text/plain');
-  var card = document.getElementById(cardId);
-  var targetList = event.target.closest('.card-list');
-  targetList.appendChild(card);
-}
+  function drop(event) {
+    event.preventDefault();
+    var cardId = event.dataTransfer.getData('text/plain');
+    var card = document.getElementById(cardId);
+    var targetList = event.target.closest('.card-list');
 
-let towers = document.querySelectorAll("tower")
+    if (targetList.children.length > 0) {
+      var lastBlock = targetList.lastElementChild;
+      var currentValue = parseInt(card.getAttribute('value'));
+      var lastValue = parseInt(lastBlock.getAttribute('value'));
 
+      if (currentValue > lastValue) {
+        gameOver = true;
+        if (gameOver) {
+          Swal.fire({
+            icon: 'error',
+            title: 'Game Over',
+            text: 'You put the big block on a small block',
+            showConfirmButton: false,
+            timer: 2000,
+          });
+
+          // Reset draggable attribute for all blocks
+          var blocks = document.querySelectorAll('.block');
+          blocks.forEach(function (block) {
+            block.setAttribute('draggable', 'false');
+          });
+        }
+        return;
+      }
+    }
+
+    targetList.appendChild(card);
+    load();
+
+    // Check if all blocks are in tower 3
+    let tower3 = document.querySelector('#tower-3');
+    if (tower3.children.length === 4) {
+      Swal.fire({
+        icon: 'success',
+        title: 'You Win',
+        text: 'You finished the game',
+        showConfirmButton: false,
+        timer: 2000,
+      });
+    }
+  }
